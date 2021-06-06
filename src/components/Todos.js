eebe66fd162912a6684555c601275976ba7595e5
Washira <us_pou@hotmail.com>
2021-06-06 23:38:14 +0700

    first commit

---
+++ src/components/Todos.js
@@ -0,0 +1,89 @@
+import React, { useState, useContext, useEffect } from "react";
+import { CredentialsContext } from "../App";
+import { v4 as uuidv4 } from 'uuid';
+
+export default function Todos() {
+  const [todos, setTodos] = useState([]);
+  const [todoText, setTodoText] = useState("");
+  const [credentials] = useContext(CredentialsContext);
+  const [filter, setFilter] = useState("uncompleted");
+
+  const persist = (newTodos) => {
+    fetch(`http://localhost:4000/todos`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${credentials.username}:${credentials.password}`,
+      },
+      body: JSON.stringify(newTodos),
+    }).then(() => {});
+  };
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/todos`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${credentials.username}:${credentials.password}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((todos) => setTodos(todos));
+  }, []);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (!todoText) return;
+    const newTodo = { id: uuidv4(), checked: false, text: todoText };
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+    setTodoText("");
+    persist(newTodos);
+  };
+  const toggleTodo = (id) => {
+    const newTodoList = [...todos];
+    const todoItem = newTodoList.find((todo) => todo.id === id);
+    todoItem.checked = !todoItem.checked;
+    setTodos(newTodoList);
+    persist(newTodoList);
+  };
+
+  const getTodos = () => {
+    return todos.filter((todo) =>
+      filter === "completed" ? todo.checked : !todo.checked
+    );
+  };
+
+  const changeFilter = (newFilter) => {
+    console.log("we are here", newFilter);
+    setFilter(newFilter);
+  };
+
+  return (
+    <div>
+      <select value={filter} onChange={(e) => changeFilter(e.target.value)}>
+        <option value="completed">completed</option>
+        <option value="uncompleted">Uncompleted</option>
+      </select>
+      {getTodos().map((todo) => (
+        <div key={todo.id}>
+          <input
+            checked={todo.checked}
+            onChange={() => toggleTodo(todo.id)}
+            type="checkbox"
+          />
+          <label>{todo.text}</label>
+        </div>
+      ))}
+      <br />
+      <form onSubmit={addTodo}>
+        <input
+          value={todoText}
+          type="text"
+          onChange={(e) => setTodoText(e.target.value)}
+        ></input>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
